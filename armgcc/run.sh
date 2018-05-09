a3ed591cbdca03717e2f20c7748d6f73d6d18dff
zdevt <zhangtao.hawk@qq.com>
2018-05-09 10:14:35 +0800

    add gogs

--- armgcc/run.sh
+++ armgcc/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --detach -it -v /Users/devz/Downloads:/mnt/temp --name arm dafeilang2018/armgcc:latest
+docker run --detach -it -v /Users/devz/Downloads:/mnt/temp --name arm -p 10022:22 dafeilang2018/armgcc:latest
