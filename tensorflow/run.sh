7d214556c562e65853d5e743dcf66d2253f487e7
zdevt <zhangtao.hawk@qq.com>
2018-05-11 11:56:10 +0800

    docker tensorflow tensorboard

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-docker run -it --name tensorflow \
+docker run -it \
+  --name tensorflow \
+  -d \
   --rm \
   -v /Users/devz/src_git/ztdocker/tensorflow/notebooks:/notebooks \
-  -p  8888:8888 tensorflow/tensorflow
+  -v /Users/devz/src_git/ztdocker/tensorflow/logs:/logs \
+  -p  6006:6006 \
+  -p  8888:8888 \
+  -e "PASSWORD=aaaaaa" \
+  tensorflow/tensorflow
+
+docker exec -d tensorflow bash -c "tensorboard --logdir /logs"
