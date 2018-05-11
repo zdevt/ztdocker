8d9f31986c37e9cc4023f020b714c8ed39d1b322
zdevt <zhangtao.hawk@qq.com>
2018-05-11 13:50:30 +0800

    start ssh

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-docker run --detach -it -v /Users/devz/Downloads:/mnt/temp --name ubuntu dafeilang2018/ubuntu
+docker run --detach \
+  -it \
+  -v /Users/devz/Downloads:/mnt/temp \
+  --name ubuntu \
+  dafeilang2018/ubuntu
+
+docker exec -d ubuntu bash -c "service ssh start"
