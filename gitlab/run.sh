80309c331a3a98de55575a8a4ca6dff04bbf199d
zdevt <zhangtao.hawk@qq.com>
2018-06-14 16:48:09 +0800

    update pwd

--- gitlab/run.sh
+++ gitlab/run.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
+#       FileName:  run.sh
+#
+#    Description:
+#
+#        Version:  1.0
+#        Created:  2018-06-14 16:42:32
+#  Last Modified:  2018-06-14 16:46:03
+#       Revision:  none
+#       Compiler:  gcc
+#
+#         Author:  zt ()
+#   Organization:
+
+PROJPWD=`pwd`
 
 docker run --detach --hostname zt \
   --publish 443:443 --publish 80:80 --publish 28888:22 \
   --rm \
   --name gitlab \
-  -v /Users/devz/src_git/ztdocker/gitlab/config:/etc/gitlab \
-  -v /Users/devz/src_git/ztdocker/gitlab/logs:/var/log/gitlab \
-  -v /Users/devz/src_git/ztdocker/gitlab/data:/var/opt/gitlab \
+  -v $PROJPWD/config:/etc/gitlab \
+  -v $PROJPWD/logs:/var/log/gitlab \
+  -v $PROJPWD/data:/var/opt/gitlab \
   gitlab/gitlab-ce:latest
--- redis/run.sh
+++ redis/run.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
+#       FileName:  run.sh
+#
+#    Description:
+#
+#        Version:  1.0
+#        Created:  2018-06-14 16:42:32
+#  Last Modified:  2018-06-14 16:47:15
+#       Revision:  none
+#       Compiler:  gcc
+#
+#         Author:  zt ()
+#   Organization:
+
+PROJPWD=`pwd`
+
 docker run -d \
   --name redis \
   --rm \
   -p 16379:6379 \
-  -v /Users/devz/src_git/ztdocker/redis/data:/data \
+  -v $PROJPWD/data:/data \
   redis
 
