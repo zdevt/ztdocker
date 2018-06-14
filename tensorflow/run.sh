b0b1907a60a41611510fdaf1c85ad5198adb82be
zdevt <zhangtao.hawk@qq.com>
2018-06-14 16:44:20 +0800

    updat pwd

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
+#       FileName:  run.sh
+#
+#    Description:
+#
+#        Version:  1.0
+#        Created:  2018-06-14 16:42:32
+#  Last Modified:  2018-06-14 16:43:19
+#       Revision:  none
+#       Compiler:  gcc
+#
+#         Author:  zt ()
+#   Organization:
+
+PROJPWD=`pwd`
+
 docker run -it \
   --name tensorflow \
   --restart always \
   -d \
-  -v /home/zt/docker:/notebooks \
-  -v /home/zt/docker:/logs \
+  -v $PROJPWD/notebooks:/notebooks \
+  -v $PROJPWD/logs:/logs \
   -p  6006:6006 \
   -p  8888:8888 \
   -p  20022:22 \
   -e "PASSWORD=aaaaaa" \
   dafeilang2018/tensorflow
 
-docker exec -d tensorflow bash -c "tensorboard --logdir /logs"
+#docker exec -d tensorflow bash -c "tensorboard --logdir /logs"
 docker exec -d tensorflow bash -c "service ssh start"
